25fc9ee58cb1ab0a15f68bf3469a7eb6cba586f5
DangKhoaK4 <zuki101004@gmail.com>
2026-05-22 22:10:43 +0700

    Initial commit

---
+++ src/main/java/swp391/aistudyhub/Su26Swp391AiStudyHubApplication.java
@@ -0,0 +1,13 @@
+package swp391.aistudyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Su26Swp391AiStudyHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Su26Swp391AiStudyHubApplication.class, args);
+    }
+
+}
