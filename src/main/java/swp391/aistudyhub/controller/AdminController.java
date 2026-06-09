05a215ac7669eb3496fe522303a9b031bbcb233d
DangKhoaK4 <zuki101004@gmail.com>
2026-06-09 23:40:24 +0700

    View user account

---
+++ src/main/java/swp391/aistudyhub/controller/AdminController.java
@@ -0,0 +1,13 @@
+package swp391.aistudyhub.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@CrossOrigin("*")
+@Tag(name = "Admin Dashboard", description = "View User Account")
+public class AdminController {
+}
