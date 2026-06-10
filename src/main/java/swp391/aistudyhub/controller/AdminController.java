b909a85bce6029ffe0783a3709d05188e8b39784
DangKhoaK4 <zuki101004@gmail.com>
2026-06-10 20:18:44 +0700

    View user account

--- src/main/java/swp391/aistudyhub/controller/AdminController.java
+++ src/main/java/swp391/aistudyhub/controller/AdminController.java
@@ -1,13 +1,29 @@
 package swp391.aistudyhub.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import swp391.aistudyhub.entity.User;
+import swp391.aistudyhub.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
 @CrossOrigin("*")
 @Tag(name = "Admin Dashboard", description = "View User Account")
 public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/account")
+    public ResponseEntity<List<User>> getAllUser() {
+        return ResponseEntity.ok().body(userService.getAllUserAccount());
+    }
+
 }
