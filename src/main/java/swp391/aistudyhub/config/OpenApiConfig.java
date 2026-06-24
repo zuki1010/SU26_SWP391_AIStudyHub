9490d5190e0521b344ec0f3a856e9bd78a247263
cuongnc195209-arch <cuongnc195209@gmail.com>
2026-06-24 21:06:52 +0700

    fix cloud

--- src/main/java/swp391/aistudyhub/config/OpenApiConfig.java
+++ src/main/java/swp391/aistudyhub/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ public class OpenApiConfig {
                         .description("Authentication and user management endpoints")
                         .version("1.0")
                         .contact(new Contact().name("AI Study Hub Team")))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
                 .components(new Components()
                         .addSecuritySchemes(BEARER_SCHEME, new SecurityScheme()
                                 .name(BEARER_SCHEME)
