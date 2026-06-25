fa37ba8208dceffb0c9719296c2c0c60687829b3
DangKhoaK4 <zuki101004@gmail.com>
2026-06-25 16:50:03 +0700

    Revert "add forum enums"
    This reverts commit 8881a0679ebadd35734f2232d938f33235e64b8b.

--- src/main/java/swp391/aistudyhub/enums/ReportStatus.java
+++
@@ -1,7 +0,0 @@
-package swp391.aistudyhub.enums;
-
-public enum ReportStatus {
-    PENDING,
-    RESOLVED,
-    DISMISSED
-}
--- src/main/java/swp391/aistudyhub/enums/TargetType.java
+++
@@ -1,6 +0,0 @@
-package swp391.aistudyhub.enums;
-
-public enum TargetType {
-    POST,
-    COMMENT
-}
