353f94718e5ecbdf31160a7d7f6e472c1e7c4a0b
DangKhoaK4 <zuki101004@gmail.com>
2026-07-19 15:12:36 +0700

    member

--- src/main/java/swp391/aistudyhub/dto/projection/DocumentResponse.java
+++ src/main/java/swp391/aistudyhub/dto/projection/DocumentResponse.java
@@ -1,5 +1,6 @@
 package swp391.aistudyhub.dto.projection;
 
+import org.springframework.beans.factory.annotation.Value;
 import swp391.aistudyhub.enums.UserRole;
 import swp391.aistudyhub.repository.AdminProfileRepository;
 import swp391.aistudyhub.repository.CustomerProfileRepository;
@@ -16,35 +17,12 @@ public interface DocumentResponse {
 
     Instant getCreatedAt();
 
+    @Value("#{target.user?.id}")
     UUID getUserId();
 
-    default String getUserEmail(UserRepository userrepo) {
-        if (getUserId() == null) return "N/A";
-        return userrepo.findUserById(getUserId())
-                .map(user -> user.getEmail())
-                .orElse("N/A");
-    }
+    @Value("#{target.user?.customerProfile != null ? target.user.customerProfile.fullName : target.user?.moderatorProfile?.fullName}")
+    String getUserFullName();
 
-    default String getUserName(UserRepository userRepo,
-                               CustomerProfileRepository customerRepo,
-                               ModeratorProfileRepository moderatorRepo,
-                               AdminProfileRepository adminRepo) {
-        if (getUserId() == null) return "Unknown";
-
-        return userRepo.findById(getUserId()).map(user -> {
-            UserRole role = user.getRole();
-
-            if ("CUSTOMER".equals(role.name())) {
-                return customerRepo.findByUser_Id(user.getId())
-                        .map(cp -> cp.getFullName()).orElse("Unknown Customer");
-            } else if ("MODERATOR".equals(role.name())) {
-                return moderatorRepo.findByUser_Id(user.getId())
-                        .map(mp -> mp.getFullName()).orElse("Unknown Moderator");
-            } else if ("ADMIN".equals(role.name())) {
-                return adminRepo.findByUser_Id(user.getId())
-                        .map(ap -> ap.getFullName()).orElse("Unknown Admin");
-            }
-            return "Unknown Role";
-        }).orElse("User Not Found");
-    }
+    @Value("#{target.user?.email}")
+    String getUserEmail();
 }
