5c61448fbaca6eb58d9302288ecbd0399e6e21d4
tovinh1408 <tovinh1408@gmail.com>
2026-07-19 13:05:48 +0700

    .

--- src/main/java/swp391/aistudyhub/controller/AdminController.java
+++ src/main/java/swp391/aistudyhub/controller/AdminController.java
@@ -23,43 +23,43 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
-    @GetMapping("/account")
-    public ResponseEntity<Page<UserAccountResponse>> getAllUser(@RequestParam(required = false) String key,
-                                                                @RequestParam(defaultValue = "0") int page,
-                                                                @RequestParam(defaultValue = "10") int size) {
-        return ResponseEntity.ok().body(adminService.getAllCustomer(key, page, size));
-    }
-
-    @PutMapping("/account/status/{id}")
-    public ResponseEntity<?> updateUserStatus(@PathVariable("id") UUID userId,
-                                              @RequestParam AccountStatus status) {
-        return ResponseEntity.ok().body(adminService.updateUserStatus(userId, status));
-    }
-
-    @GetMapping("/document")
-    public ResponseEntity<?> getAllDocument(@RequestParam(defaultValue = "0") int page,
-                                                     @RequestParam(defaultValue = "10") int size
-    ) {
-        return ResponseEntity.ok().body(adminService.getAllDocument(page, size));
-    }
-
-    @PutMapping("/account/role/{id}")
-    public ResponseEntity<?> updateUserRole(@PathVariable("id") UUID userId,
-                                            @RequestParam UserRole role) {
-        return ResponseEntity.ok(adminService.updateUserRole(userId, role));
-    }
-
-    @GetMapping("/chat")
-    public ResponseEntity<?> getAllChatToDay(@RequestParam(defaultValue = "0") int page,
-                                             @RequestParam(defaultValue = "10") int size) {
-        return ResponseEntity.ok(adminService.getAllChat(page, size));
-    }
-
-    @GetMapping("/storage")
-    public ResponseEntity<?> getAllStorageUsage(@RequestParam(defaultValue = "0") int page,
-                                                @RequestParam(defaultValue = "10") int size) {
-        return ResponseEntity.ok(adminService.getAllStorage(page, size));
-    }
+//    @GetMapping("/account")
+//    public ResponseEntity<Page<UserAccountResponse>> getAllUser(@RequestParam(required = false) String key,
+//                                                                @RequestParam(defaultValue = "0") int page,
+//                                                                @RequestParam(defaultValue = "10") int size) {
+//        return ResponseEntity.ok().body(adminService.getAllCustomer(key, page, size));
+//    }
+//
+//    @PutMapping("/account/status/{id}")
+//    public ResponseEntity<?> updateUserStatus(@PathVariable("id") UUID userId,
+//                                              @RequestParam AccountStatus status) {
+//        return ResponseEntity.ok().body(adminService.updateUserStatus(userId, status));
+//    }
+//
+//    @GetMapping("/document")
+//    public ResponseEntity<?> getAllDocument(@RequestParam(defaultValue = "0") int page,
+//                                                     @RequestParam(defaultValue = "10") int size
+//    ) {
+//        return ResponseEntity.ok().body(adminService.getAllDocument(page, size));
+//    }
+//
+//    @PutMapping("/account/role/{id}")
+//    public ResponseEntity<?> updateUserRole(@PathVariable("id") UUID userId,
+//                                            @RequestParam UserRole role) {
+//        return ResponseEntity.ok(adminService.updateUserRole(userId, role));
+//    }
+//
+//    @GetMapping("/chat")
+//    public ResponseEntity<?> getAllChatToDay(@RequestParam(defaultValue = "0") int page,
+//                                             @RequestParam(defaultValue = "10") int size) {
+//        return ResponseEntity.ok(adminService.getAllChat(page, size));
+//    }
+//
+//    @GetMapping("/storage")
+//    public ResponseEntity<?> getAllStorageUsage(@RequestParam(defaultValue = "0") int page,
+//                                                @RequestParam(defaultValue = "10") int size) {
+//        return ResponseEntity.ok(adminService.getAllStorage(page, size));
+//    }
 
     @PutMapping("/config-storage")
     public ResponseEntity<?> configureTotalStorageQuota() {
